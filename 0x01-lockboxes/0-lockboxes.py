f8349327ca1d318d47aa74ae9a6a5add041d4267
n1o2h <aythmadnhylt@gmail.com>
2024-10-14 15:34:16 +0200

    task done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""BOXES"""
+
+def canUnlockAll(boxes):
+    total_boxes = len(boxes)
+    setofkeys = [0]
+    counter = 0
+    index = 0
+
+    while index < len(setofkeys):
+        setkey = setofkeys[index]
+        for key in boxes[setkey]:
+            if 0 < key < total_boxes and key not in setofkeys:
+                setofkeys.append(key)
+                counter += 1
+        index += 1
+
+    return counter == total_boxes - 1
